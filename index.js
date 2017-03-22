04e4b5f2e9e1dae79212c728cb8acfc66cdd7711
NellyWhads <nellywhads@gmail.com>
2017-03-22 00:03:25 -0400

    Figuring this out

--- index.js
+++ index.js
@@ -156,6 +156,7 @@ function callSendAPI(messageData) {
     if (!error && response.statusCode == 200) {
       var recipientId = body.recipient_id;
       var messageId = body.message_id;
+      console.log("What even is this: ", body);
 
       console.log("Successfully sent generic message with id %s to recipient %s", 
         messageId, recipientId);
