1089b40c492f46d2e893a1a3c97a1de4ccd0bc9d
NellyWhads <nellywhads@gmail.com>
2017-03-21 22:25:03 -0400

    Basic message logging

--- index.js
+++ index.js
@@ -3,16 +3,45 @@ var app = express();
 
 app.set('port', process.env.PORT || 5000);
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.send('Hey, I\'m a chatbot for Bittrex.');
 });
 
-app.get('/webhook', function (req, res) {
+app.get('/webhook', function(req, res) {
     if (req.query['hub.verify_token'] === VERIFY_TOKEN) {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
 });
+
+app.post('/webhook', function(req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+    res.sendStatus(200);
+  }
+});
+  
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  console.log("Message data: ", event.message);
+}
 
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
