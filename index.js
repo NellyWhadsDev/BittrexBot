4231777b80854b2250bf9fd803a9f6843b3cfaa3
NellyWhads <nellywhads@gmail.com>
2017-03-22 00:05:15 -0400

    Can we still access this here?

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ function callSendAPI(messageData) {
     if (!error && response.statusCode == 200) {
       var recipientId = body.recipient_id;
       var messageId = body.message_id;
-      console.log("What even is this: ", body);
+      console.log("What even is this: ", messageData);
 
       console.log("Successfully sent generic message with id %s to recipient %s", 
         messageId, recipientId);
